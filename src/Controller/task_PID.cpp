9fde3f976519d19f294687fda659e465f7dc4578
Ryan McLaughlin <rmclaughlin168@gmail.com>
2021-11-10 13:29:53 -0800

    changed printing method for PID task

--- src/Controller/task_PID.cpp
+++ src/Controller/task_PID.cpp
@@ -45,7 +45,7 @@ void task_PID(void* p_params)
         float controlRollSpeed  = rollController.Update( rollSetpoint, rollAngle.get() );
         float controlYawSpeed   = yawController.Update( yawSetpoint, yawAngle.get() );
         // Serial.println("controller updates have been made");
-        Serial.printf("Cont: p = %f, r = %f, y = %f",controlPitchSpeed, controlRollSpeed, controlYawSpeed);
+        Serial << "Cont: p = " << controlPitchSpeed << "r = " << controlRollSpeed << "y = " << controlYawSpeed << endl;
 
         // put controller outputs into shares
         pMotSpeed.put(controlPitchSpeed);
