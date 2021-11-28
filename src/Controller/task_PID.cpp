aec35d920985868caf0e3a5c0168621740cf9434
Mfrosty <mtfrost@calpoly.edu>
2021-11-28 15:54:06 -0800

    removed comments/old code

--- src/Controller/task_PID.cpp
+++ src/Controller/task_PID.cpp
@@ -33,12 +33,6 @@ void task_PID(void* p_params)
     rollController.Init();
     yawController.Init(); 
 
-    // Preliminary Testing
-    // SetInputs             (  Kp,   Ki,    Kd,  tau,     min,    max, minInt, maxInt,     T)                   
-    // pitchController.SetInputs(4.5f, 1.0f, 18.0f, 1.0f, -100.0f, 100.0f, -10.0f,  10.0f, 0.05f); // Matthew finished prelim tuning (4.5, 1.0, 18.0)
-    // rollController.SetInputs (1.5f, 0.5f,  4.0f, 1.0f,  -75.0f,  75.0f, -10.0f,  10.0f, 0.05f); // Matthew finished prelim tuning (1.5, 0.5, 4.0)
-    // yawController.SetInputs  (5.0f, 0.5f, 25.0f, 1.0f, -150.0f, 150.0f, -10.0f,  10.0f, 0.05f); // Matthew finished prelim tuning (5.0, 0.5, 25.0)
-    
     // Testing before reducing Tau
     // SetInputs             (  Kp,   Ki,    Kd,  tau,     min,    max, minInt, maxInt,     T)                   
     // pitchController.SetInputs(5.0f, 2.0f, 20.0f, 1.0f, -100.0f, 100.0f, -10.0f,  10.0f, 0.05f); // Matthew finished tuning with phone (5.0, 2.0, 20.0)
@@ -51,9 +45,9 @@ void task_PID(void* p_params)
     yawController.SetInputs  (20.0f, 0.0f,  0.0f, 0.5f, -250.0f, 250.0f, -10.0f,  10.0f, 0.05f); // Matthew finished tuning with phone (1.0, 0.5, 6.0)
 
     // input setpoints for each motor here
-    float pitchSetpoint =  -90.0f; // should be -90 
-    float rollSetpoint  =   0.0f;  // should be 0
-    float yawSetpoint   =   0.0f;  // should be 0
+    float pitchSetpoint =  -90.0f; // should be -90 for the BNO055
+    float rollSetpoint  =   0.0f;  // should be 0 for the BNO055
+    float yawSetpoint   =   0.0f;  // should be 0 for the BNO055
 
     for (;;)
     {
@@ -61,7 +55,6 @@ void task_PID(void* p_params)
         float controlPitchSpeed = pitchController.Update( pitchSetpoint, pitchAngle.get() );
         float controlRollSpeed  = rollController.Update( rollSetpoint, rollAngle.get() );
         float controlYawSpeed   = yawController.Update( yawSetpoint, yawAngle.get() );
-        // Serial << "Cont: p = " << controlPitchSpeed << " r = " << controlRollSpeed << " y = " << controlYawSpeed << endl;
         // Serial << " r = " << controlRollSpeed << ", angle = " << rollAngle.get() << endl;
         Serial << " p = " << controlPitchSpeed << ", angle = " << pitchAngle.get() << endl;
         // Serial << " y = " << controlYawSpeed << ", angle = " << yawAngle.get() << endl;
