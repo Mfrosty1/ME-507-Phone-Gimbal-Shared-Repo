4a3ccecae3ea6f0b8285ec4a23b13b970894372e
Mfrosty <mtfrost@calpoly.edu>
2021-12-02 15:44:16 -0800

    test 9

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Stabilizing Phone Gimbal", "index.html", [
+  [ "Phone Gimbal", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
